154b4813484437ab6cdb5d94392f4e0c929fcb86
Mathis Bullinger <mathis@bullinger.dev>
2020-08-31 12:56:33 +0200

    feat: remove pronunciation from wiki wiki extracts

--- src/utils/wiki.ts
+++ src/utils/wiki.ts
@@ -7,9 +7,32 @@ export const fetchArticle = async (name: string) => {
       name
     )}&origin=*`
   ).then((res) => res.json())
-  return (cache[name] = Object.entries(query.pages).find(
+  const data: any = Object.entries(query.pages).find(
     ([k, v]: [string, any]) => v && k === v.pageid?.toString()
-  )?.[1])
+  )?.[1]
+
+  if (data.extract) {
+    const node = document.createElement('div')
+    node.innerHTML = data.extract
+    node
+      .querySelectorAll('[title~=pronunciation]')
+      .forEach((node) => node.remove())
+
+    const removeEmpty = (parent: HTMLElement) => {
+      Array.from((parent.children as unknown) as HTMLElement[]).forEach(
+        (node) => {
+          removeEmpty(node)
+          if (/^\s*$/.test((node as HTMLElement).innerHTML))
+            return node.remove()
+        }
+      )
+    }
+    removeEmpty(node)
+
+    data.extract = node.innerHTML.replace(/\(\s*\)/g, '')
+  }
+
+  return (cache[name] = data)
 }
 
 export const read = (name: string) => cache[name]
