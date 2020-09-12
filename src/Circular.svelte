d5ce917f7a8d28c593ae5b535d5dc04263c5d347
Mathis Bullinger <mathis@bullinger.dev>
2020-09-12 20:12:46 +0200

    perf: interval merging & throttling

--- src/Circular.svelte
+++ src/Circular.svelte
@@ -1,9 +1,8 @@
 <script lang="ts">
   import intervals from './utils/intervals'
+  import throttle from 'lodash/throttle'
 
   const levels = intervals().map((lvl) => lvl.filter(({ name }) => name))
-
-  $: console.log(levels)
 
   let min = levels[0][0].start
   let max = levels[0].slice(-1)[0].end
@@ -11,9 +10,7 @@
   $: scaling = 1
 
   $: pos = (r: number, v: number) => {
-    v = Math.min(1, ((Math.max(v, min) - min) / (max - min)) ** scaling)
-
-    const gap = 0.03
+    const gap = 0.01
 
     return [
       50 + Math.sin(v * 2 * -Math.PI * (1 - gap) - (Math.PI * gap) / 2) * r,
@@ -36,22 +33,87 @@
 
   $: layers = levels.map((level, i) => {
     const r = radius(i)
-    return level
-      .map(({ start, end, color }) => {
-        const offset = 0.0001
-        const [x1, y1] = pos(r, start - (max - min) * offset)
-        const [x2, y2] = pos(r, end + (max - min) * offset)
-        if (Math.abs(x2 - x1) + Math.abs(y2 - y1) < 0.15) return
-        return {
-          d: `M ${x1} ${y1} A ${r} ${r} 0 ${
-            (x2 - x1) * (50 - y1) - (y2 - y1) * (50 - x1) > 0 ? 0 : 1
-          } 1 ${x2} ${y2}`,
-          color,
-          width: _spaceDist[i] * 45 - 1,
-        }
-      })
-      .filter(Boolean)
+    const lvl = level.map(({ start, end, color }) => {
+      const offset = 0.0001
+      const startRel = Math.min(
+        1,
+        ((Math.max(start - (max - min) * offset, min) - min) / (max - min)) **
+          scaling
+      )
+      const endRel = Math.min(
+        1,
+        ((Math.max(end + (max - min) * offset, min) - min) / (max - min)) **
+          scaling
+      )
+
+      return {
+        start,
+        end,
+        startRel,
+        endRel,
+        color,
+        width: _spaceDist[i] * 45 - 1,
+      }
+    })
+
+    const getParent = ({ start, end }) =>
+      i === 0
+        ? null
+        : levels[i - 1].find((l) => l.start <= start && l.end >= end)
+
+    let curParent = getParent(lvl[lvl.length - 1])
+
+    for (let i = lvl.length - 1; i > 0; i--) {
+      const nextParent = getParent(lvl[i - 1])
+
+      if (curParent) {
+        const skip = curParent !== nextParent
+        curParent = nextParent
+        if (skip) continue
+      }
+
+      if (lvl[i].endRel - lvl[i - 1].startRel >= 0.01) continue
+      lvl[i].startRel = lvl[i - 1].startRel
+      lvl.splice(i - 1, 1)
+    }
+
+    while (lvl.length >= 2 && lvl[0].endRel - lvl[0].startRel < 0.005) {
+      lvl[1].startRel = lvl[0].startRel
+      lvl.splice(0, 1)
+    }
+    while (
+      lvl.length >= 2 &&
+      lvl[lvl.length - 1].endRel - lvl[lvl.length - 1].startRel < 0.005
+    ) {
+      lvl[lvl.length - 2].endRel = lvl[lvl.length - 1].endRel
+      lvl.splice(lvl.length - 1, 1)
+    }
+
+    return lvl.map(({ startRel, endRel, ...rest }) => {
+      const [x1, y1] = pos(r, startRel)
+      const [x2, y2] = pos(r, endRel)
+      return {
+        d: `M ${x1} ${y1} A ${r} ${r} 0 ${
+          (x2 - x1) * (50 - y1) - (y2 - y1) * (50 - x1) > 0 ? 0 : 1
+        } 1 ${x2} ${y2}`,
+        ...rest,
+      }
+    })
   })
+
+  let scrollAcc = 0
+
+  const applyScroll = throttle(
+    () => {
+      scaling = Math.max(1, scaling + scaling * (scrollAcc / 1000))
+      scrollAcc = 0
+    },
+    1000 / 30,
+    {
+      leading: false,
+      trailing: true,
+    }
+  )
 
   function scroll(e: WheelEvent) {
     e.preventDefault()
@@ -67,7 +129,14 @@
       wheelDeltaY *= 5
     }
 
-    scaling = Math.max(1, scaling + scaling * (wheelDeltaY / 1000))
+    if (
+      'chrome' in window && wheelDeltaY
+        ? wheelDeltaY === -3 * e.deltaY
+        : e.deltaMode === 0
+    ) {
+      scrollAcc += wheelDeltaY
+      applyScroll()
+    } else scaling = Math.max(1, scaling + scaling * (wheelDeltaY / 1000))
   }
 </script>
 
