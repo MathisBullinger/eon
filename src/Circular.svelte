53e26ea7d2da1f0370450d77a89bc9a7feea5530
Mathis Bullinger <mathis@bullinger.dev>
2020-09-12 18:51:35 +0200

    feat: circular zoom

--- src/Circular.svelte
+++ src/Circular.svelte
@@ -3,16 +3,23 @@
 
   const levels = intervals().map((lvl) => lvl.filter(({ name }) => name))
 
+  $: console.log(levels)
+
   let min = levels[0][0].start
   let max = levels[0].slice(-1)[0].end
-  const span = max - min
-  min -= span * 0.01
-  max += span * 0.01
 
-  const pos = (r: number, v: number) => [
-    50 + Math.sin(((v - min) / (max - min)) * 2 * -Math.PI) * r,
-    50 + Math.cos(((v - min) / (max - min)) * 2 * -Math.PI) * r,
-  ]
+  $: scaling = 1
+
+  $: pos = (r: number, v: number) => {
+    v = Math.min(1, ((Math.max(v, min) - min) / (max - min)) ** scaling)
+
+    const gap = 0.03
+
+    return [
+      50 + Math.sin(v * 2 * -Math.PI * (1 - gap) - (Math.PI * gap) / 2) * r,
+      50 + Math.cos(v * 2 * -Math.PI * (1 - gap) - (Math.PI * gap) / 2) * r,
+    ]
+  }
 
   const lastToFirstRatio = 4
 
@@ -27,12 +34,40 @@
 
   const radius = (lvl: number) => 5 + _relPos[lvl] * 45
 
-  const segment = (lvl: number, start: number, end: number) => {
-    const r = radius(lvl)
-    const [x1, y1] = pos(r, start - span * 0.001)
-    const [x2, y2] = pos(r, end + span * 0.001)
+  $: layers = levels.map((level, i) => {
+    const r = radius(i)
+    return level
+      .map(({ start, end, color }) => {
+        const offset = 0.0001
+        const [x1, y1] = pos(r, start - (max - min) * offset)
+        const [x2, y2] = pos(r, end + (max - min) * offset)
+        if (Math.abs(x2 - x1) + Math.abs(y2 - y1) < 0.15) return
+        return {
+          d: `M ${x1} ${y1} A ${r} ${r} 0 ${
+            (x2 - x1) * (50 - y1) - (y2 - y1) * (50 - x1) > 0 ? 0 : 1
+          } 1 ${x2} ${y2}`,
+          color,
+          width: _spaceDist[i] * 45 - 1,
+        }
+      })
+      .filter(Boolean)
+  })
 
-    return `M ${x1} ${y1} A ${r} ${r} 0 0 1 ${x2} ${y2}`
+  function scroll(e: WheelEvent) {
+    e.preventDefault()
+    let { wheelDeltaX, wheelDeltaY } = e as any
+    if (!wheelDeltaY && !wheelDeltaX) {
+      wheelDeltaX = -e.deltaX
+      wheelDeltaY = -e.deltaY
+    }
+    if (e.shiftKey) [wheelDeltaY, wheelDeltaX] = [wheelDeltaX, wheelDeltaY]
+    wheelDeltaX *= -1
+    if (e.ctrlKey) {
+      e.preventDefault()
+      wheelDeltaY *= 5
+    }
+
+    scaling = Math.max(1, scaling + scaling * (wheelDeltaY / 1000))
   }
 </script>
 
@@ -52,13 +87,10 @@
   }
 </style>
 
-<svg width={100} height={100} viewBox="0 0 100 100">
-  {#each levels as level}
-    {#each level as interval}
-      <path
-        d={segment(levels.indexOf(level), interval.start, interval.end)}
-        stroke={interval.color}
-        stroke-width={_spaceDist[levels.indexOf(level)] * 45 - 1} />
+<svg width={100} height={100} viewBox="0 0 100 100" on:wheel={scroll}>
+  {#each layers as layer}
+    {#each layer as { d, color, width }}
+      <path {d} stroke={color} stroke-width={width} />
     {/each}
   {/each}
 </svg>
